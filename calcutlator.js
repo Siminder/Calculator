66b50f0ded41f6b7b66d2a369a395b351ab7c8e1
Siminder Bansal <bansalsiminder234@gmail.com>
2026-02-26 14:28:05 +0000

    adding js for calcualtor also added basic function for calculator

---
+++ calcutlator.js
@@ -0,0 +1,28 @@
+let Num1 = 10;
+let Num2 = 5;
+let Operator =  "";
+
+
+let Add = (Num1, Num2) => {
+    return console.log(Num1 + Num2);
+}
+
+let Subtract = (Num1, Num2) => {
+    return console.log(Num1 - Num2);
+}
+
+let Mutiply = (Num1, Num2) => {
+    return console.log(Num1 * Num2);
+}
+
+
+let Divide = (Num1, Num2) => {
+    return console.log(Num1/Num2);
+}
+
+Add(Num1, Num2);
+Subtract(Num1, Num2);
+Mutiply(Num1,Num2);
+Divide(Num1, Num2)
+
+
